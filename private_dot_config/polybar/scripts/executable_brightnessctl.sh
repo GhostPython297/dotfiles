25627cc0064a2ce1438c368ceac0bbc24741b588
João <bunkhouse-dork-hut@duck.com>
2025-05-31 19:10:29 -0300

    migrando do chezmoi para o gnu stow
    percebi que fica melhor para gerenciar e compartilhar dotfiles entre
    dispositivos se eu usar uma ferramenta como o stow, pois é mais simples
    conceitualmente que o chezmoi.

--- private_dot_config/polybar/scripts/executable_brightnessctl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Obtém o brilho atual como percentagem
-output=$(brightnessctl g)
-max=$(brightnessctl m)
-percent=$((100 * output / max))
-
-# Exibe o valor com ícone (opcional)
-echo "󱠂 ${percent}%"
